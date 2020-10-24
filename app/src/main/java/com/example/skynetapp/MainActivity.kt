0bb07451738788c73ef28d2c08572a0a3a7ff242
DDamjan <d.damjan97@gmail.com>
2020-10-24 23:16:03 +0200

    Revert "Slikanje radi sa telefona"
    This reverts commit 0f1b4876ba4c1cd5c5e5b5b06a8737505f12cc10.

--- app/src/main/java/com/example/skynetapp/MainActivity.kt
+++ app/src/main/java/com/example/skynetapp/MainActivity.kt
@@ -1,209 +1,38 @@
 package com.example.skynetapp
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.app.*
-import androidx.core.content.ContextCompat
-import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
-import ai.fritz.core.Fritz
-import ai.fritz.vision.FritzVision
-import ai.fritz.vision.FritzVisionImage
-import ai.fritz.vision.ImageRotation
-import ai.fritz.vision.imagelabeling.ImageLabelManagedModelFast
-import ai.fritz.vision.PredictorStatusListener
-import ai.fritz.vision.imagelabeling.FritzVisionLabelPredictor
-import android.net.Uri
-import android.widget.TextView
-import androidx.camera.core.*
-import java.io.File
-import java.text.SimpleDateFormat
 
 class MainActivity : AppCompatActivity() {
-    private var imageCapture: ImageCapture? = null
-    private val cameraExecutor = Executors.newSingleThreadExecutor()
-    private lateinit var outputDirectory: File
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
-        Fritz.configure(this, "84e387eb963943cb9a6163e5a4642d9f");
-
-        if (allPermissionsGranted()) {
-            startCamera()
-        } else {
-            ActivityCompat.requestPermissions(
-                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
-        }
-
-        camera_capture_button.setOnClickListener { takePhoto() }
-
-        outputDirectory = getOutputDirectory()
-
-    }
-
-    private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
-        cameraProviderFuture.addListener(Runnable {
-            // Used to bind the lifecycle of cameras to the lifecycle owner
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-            // Preview
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(viewFinder.createSurfaceProvider())
-                }
-
-            // Select back camera as a default
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll()
-
-                // Bind use cases to camera
-                cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture)
-
-            } catch(exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
-            }
-
-        }, ContextCompat.getMainExecutor(this))
-
-        imageCapture = ImageCapture.Builder()
-            .build()
-//        ==============================================================================================CLIENT PREDICTOR CODE===================================
-//        val imageAnalyzer = ImageAnalysis.Builder()
-//            .build()
-//            .also {
-//                it.setAnalyzer(cameraExecutor, ImageProcessor ())
-//            }
-//        ==============================================================================================CLIENT PREDICTOR CODE===================================
-    }
-
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(
-            baseContext, it) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun takePhoto() {
-        // Get a stable reference of the modifiable image capture use case
-        val imageCapture = imageCapture ?: return
-
-        // Create time-stamped output file to hold the image
-        val photoFile = File(
-            outputDirectory,
-            SimpleDateFormat(FILENAME_FORMAT, Locale.US
-            ).format(System.currentTimeMillis()) + ".jpg")
-
-        // Create output options object which contains file + metadata
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
-
-        // Set up image capture listener, which is triggered after photo has
-        // been taken
-        imageCapture.takePicture(
-            outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
-                override fun onError(exc: ImageCaptureException) {
-                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
-                }
-
-                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                    val savedUri = Uri.fromFile(photoFile)
-                    val msg = "Photo capture succeeded: $savedUri"
-                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                    Log.d(TAG, msg)
-                }
-            })
-    }
-
-    private fun getOutputDirectory(): File {
-        val mediaDir = externalMediaDirs.firstOrNull()?.let {
-            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
-        return if (mediaDir != null && mediaDir.exists())
-            mediaDir else filesDir
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String>, grantResults:
-        IntArray) {
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera()
-            } else {
-                Toast.makeText(this,
-                    "Permissions not granted by the user.",
-                    Toast.LENGTH_SHORT).show()
-                finish()
-            }
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        cameraExecutor.shutdown()
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 
-//        ==============================================================================================CLIENT PREDICTOR CODE===================================
-//    inner class ImageProcessor : ImageAnalysis.Analyzer {
-//        var predictor: FritzVisionLabelPredictor? = null
-//        val TAG = javaClass.simpleName
-//
-//        @SuppressLint("UnsafeExperimentalUsageError")
-//        override fun analyze(image: ImageProxy) {
-//
-//            //Handle all the ML logic here
-//            val mediaImage = image.image
-//
-//            val imageRotation = ImageRotation.getFromValue(image.imageInfo.rotationDegrees)
-//
-//            val visionImage = FritzVisionImage.fromMediaImage(mediaImage, imageRotation)
-//
-//            val managedModel = ImageLabelManagedModelFast()
-//
-//            FritzVision.ImageLabeling.loadPredictor(
-//                managedModel,
-//                object : PredictorStatusListener<FritzVisionLabelPredictor> {
-//                    override fun onPredictorReady(p0: FritzVisionLabelPredictor?) {
-//                        Log.d(TAG, "Image Labeling predictor is ready")
-//                        predictor = p0
-//                    }
-//                })
-//
-//            val labelResult = predictor?.predict(visionImage)
-//
-//                labelResult?.resultString?.let {
-//                    val sname = it.split(":")
-//                    Log.e(TAG, it)
-//                    Log.e(TAG, sname[0])
-//                    println(sname[0])
-//                    tv_name.text = sname[0]
-//                } ?: kotlin.run {
-//                    tv_name.visibility = TextView.INVISIBLE
-//                }
-//        }
-//    }
-//        ==============================================================================================CLIENT PREDICTOR CODE===================================
-
-    companion object {
-        private const val TAG = "CameraXBasic"
-        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
