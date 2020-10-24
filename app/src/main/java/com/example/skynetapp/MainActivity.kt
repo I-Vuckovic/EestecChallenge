02d991fe5caaf7b484174cec0d60f379805e3ccf
Gardrep <gardrep@gmail.com>
2020-10-24 22:23:58 +0200

    TTS implemented
    With edit text box

--- app/src/main/java/com/example/skynetapp/MainActivity.kt
+++ app/src/main/java/com/example/skynetapp/MainActivity.kt
@@ -1,23 +1,52 @@
 package com.example.skynetapp
 
+import android.content.DialogInterface
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import android.speech.tts.TextToSpeech
+import android.speech.tts.TextToSpeech.OnInitListener
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+    var t1: TextToSpeech? = null
+    var ed1: EditText? = null
+    var b1: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setContentView(R.layout.tts_main)
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        ed1 = findViewById<View>(R.id.editText) as EditText
+        b1 = findViewById<View>(R.id.button) as Button
+
+        t1 = TextToSpeech(applicationContext,
+            OnInitListener { status ->
+                if (status != TextToSpeech.ERROR) {
+                    t1!!.language = Locale.UK
+                }
+            })
+
+        b1?.setOnClickListener()
+        {
+            val toSpeak = ed1!!.text.toString()
+            Toast.makeText(applicationContext, toSpeak, Toast.LENGTH_SHORT).show()
+            t1!!.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null)
         }
+    }
+
+    override fun onPause() {
+        if (t1 != null) {
+            t1!!.stop()
+            t1!!.shutdown()
+        }
+        super.onPause()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -35,4 +64,8 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+}
+
+private fun Button.setOnClickListener(onClickListener: DialogInterface.OnClickListener) {
+
 }
